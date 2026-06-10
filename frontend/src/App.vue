c78d4b3fc12a362dcfada1d5cb9404448dceae41
aixinjueluo yangzhi <2242787669@qq.com>
2026-06-10 21:14:12 +0800

    实现用户登录、注册及云端持久化

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,38 @@
-<script setup lang="ts">
+<script setup>
+import NavBar from "@/components/navbar/NavBar.vue";
+import {onMounted} from "vue";
+import {useUserStore} from "@/stores/user.js";
+import api from "@/js/http/api.js";
+import {useRoute, useRouter} from "vue-router";
 
-import NarBar from "@/components/navbar/NarBar.vue";
+const user = useUserStore()
+const route = useRoute()
+const router = useRouter()
+
+onMounted(async () => {
+  try {
+    const res = await api.get('/api/user/account/get_user_info/')
+    const data = res.data
+    if (data.result === 'success') {
+      user.setUserInfo(data)
+    }
+  } catch (err) {
+  } finally {
+    user.setHasPulledUserInfo(true)
+
+    if (route.meta.needLogin && !user.isLogin()) {
+      await router.replace({
+        name: 'user-account-login-index',
+      })
+    }
+  }
+})
 </script>
 
 <template>
-  <NarBar>
+  <NavBar>
     <RouterView/>
-  </NarBar>
+  </NavBar>
 </template>
 
 <style scoped>
