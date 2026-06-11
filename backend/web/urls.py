e6834df3edeebdca007f3455efbcbc8c50ccc791
aixinjueluo yangzhi <2242787669@qq.com>
2026-06-11 22:17:44 +0800

    实现编辑资料

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path, re_path
 
+from web.views.create.character.create import CreateCharacterView
+from web.views.create.character.get_single import GetSingleCharacterView
+from web.views.create.character.remove import RemoveCharacterView
+from web.views.create.character.update import UpdateCharacterView
 from web.views.index import index
 
 from web.views.user.account.get_user_info import GetUserInfoView
@@ -11,6 +11,7 @@ from web.views.user.account.login import LoginView
 from web.views.user.account.logout import LogoutView
 from web.views.user.account.refresh_token import RefreshTokenView
 from web.views.user.account.register import RegisterView
+from web.views.user.profile.updata import UpdateProfileView
 
 urlpatterns = [
     path('api/user/account/login/', LoginView.as_view()),
@@ -18,5 +19,13 @@ urlpatterns = [
     path('api/user/account/register/', RegisterView.as_view()),
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
+    path('api/user/profie/update/', UpdateProfileView.as_view()),
+
+    path('api/create/character/create/',CreateCharacterView.as_view()),
+    path('api/create/character/update/',UpdateCharacterView.as_view()),
+    path('api/create/character/remove/',RemoveCharacterView.as_view()),
+    path('api/create/character/get_single/',GetSingleCharacterView.as_view()),
+
     path('', index),
+    re_path(r'^(?!media/|static/|assets/).*$', index),
 ]
